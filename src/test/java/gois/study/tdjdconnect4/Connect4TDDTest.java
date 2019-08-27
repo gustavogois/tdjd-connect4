ad526d17428bd526bf2de51cdde94a9ee5ff78a6
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 22:20:18 +0100

    Requirement 2 – introducing discs This is the implementation for the second requirement.
    Players introduce discs on the top of the columns. An introduced disc drops down the board if the column is empty. Future discs introduced in the same column will stack over the previous ones.
    
    We can split this requirement into the following tests:
    
    When a disc is inserted into an empty column, its position is 0
    When a second disc is inserted into the same column, its position is 1
    When a disc is inserted into the board, the total number of discs increases
    When a disc is put outside the boundaries, a Runtime Exception is thrown
    When a disc is inserted into a column and there's no room available for it, then a Runtime Exception is thrown
    Also, these other tests are derived from the first requirement. They are related to the board limits or board behavior.

--- src/test/java/gois/study/tdjdconnect4/Connect4TDDTest.java
+++ src/test/java/gois/study/tdjdconnect4/Connect4TDDTest.java
@@ -1,20 +1,67 @@
 package gois.study.tdjdconnect4;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class Connect4TDDTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     private Connect4TDD tested;
 
     @Before
     public void beforeEachTest() {
         tested = new Connect4TDD();
     }
+
     @Test
     public void whenTheGameIsStartedTheBoardIsEmpty() {
         assertThat(tested.getNumberOfDiscs(), is(0));
+    }
+
+    @Test
+    public void whenDiscOutsideBoardThenRuntimeException() {
+        int column = -1;
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("Invalid column " + column);
+        tested.putDiscInColumn(column);
+    }
+
+    @Test
+    public void whenFirstDiscInsertedInColumnThenPositionIsZero() {
+        int column = 1;
+        assertThat(tested.putDiscInColumn(column),  is(0));
+    }
+
+    @Test
+    public void whenSecondDiscInsertedInColumnThenPositionIsOne() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column), is(1));
+    }
+
+    @Test
+    public void whenDiscInsertedThenNumberOfDiscsIncreases() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getNumberOfDiscs(), is(1));
+    }
+
+    @Test
+    public void whenNoMoreRoomInColumnThenRuntimeException() {
+        int column = 1;
+        int maxDiscsInColumn = 6; // the number of rows
+        for (int times = 0; times < maxDiscsInColumn; ++times) {
+            tested.putDiscInColumn(column);
+        }
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("No more room in column " + column);
+        tested.putDiscInColumn(column);
     }
 }
