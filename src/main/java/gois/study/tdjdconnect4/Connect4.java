b6ebdb8397f5db5750c055c11e8a0009ced7d721
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 21:38:17 +0100

    Requirement 5 – win conditions (I) The first game has a finished condition.
    When no more discs can be inserted, the game finishes and it is considered a draw.
    The following code shows one of the possible implementations

--- src/main/java/gois/study/tdjdconnect4/Connect4.java
+++ src/main/java/gois/study/tdjdconnect4/Connect4.java
@@ -69,4 +69,16 @@ public class Connect4 {
         }
         return -1;
     }
+
+    public boolean isFinished() {
+        int numOfDiscs = 0;
+        for (int col = 0; col < COLUMNS; ++col) {
+            numOfDiscs += getNumberOfDiscsInColumn(col);
+        }
+        if (numOfDiscs >= COLUMNS * ROWS) {
+            System.out.println("It's a draw");
+            return true;
+        }
+        return false;
+    }
 }
