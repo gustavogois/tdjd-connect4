c4dd608f087ba1595b98e76320d42f7ba3895cdb
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 21:45:52 +0100

    Requirement 6 – win condition (II) The first win condition.
    If a player inserts a disc and connects more than three discs of his colour in a straight vertical line, then that player wins.
    The checkWinCondition private method implements this rule by scanning whether or not the last move is a winning one

--- src/main/java/gois/study/tdjdconnect4/Connect4.java
+++ src/main/java/gois/study/tdjdconnect4/Connect4.java
@@ -2,15 +2,18 @@ package gois.study.tdjdconnect4;
 
 import java.util.Arrays;
 import java.util.StringJoiner;
+import java.util.regex.Pattern;
 
 public class Connect4 {
 
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
     private static final String DELIMITER = "|";
+    public static final int DISCS_FOR_WIN = 4;
 
     private Color[][] board = new Color[COLUMNS][ROWS];
     private Color currentPlayer = Color.RED;
+    private Color winner;
 
     public Connect4() {
         for (Color[] column : board) {
@@ -44,6 +47,7 @@ public class Connect4 {
             if (numOfDiscs < ROWS) {
                 board[column - 1][numOfDiscs] = currentPlayer;
                 printBoard();
+                checkWinCondition(column - 1, numOfDiscs);
                 switchPlayer();
             } else {
                 System.out.println(numOfDiscs);
@@ -54,6 +58,21 @@ public class Connect4 {
         } else {
             System.out.println("Column out of bounds");
             printBoard();
+        }
+    }
+
+    private void checkWinCondition(int col, int row) {
+        Pattern winPattern = Pattern.compile(".*" +
+                currentPlayer + "{" + DISCS_FOR_WIN + "}.*");
+
+        // Vertical check
+        StringJoiner stringJoiner = new StringJoiner("");
+        for (int auxRow = 0; auxRow < ROWS; ++auxRow) {
+            stringJoiner.add(board[col][auxRow].toString());
+        }
+        if (winPattern.matcher(stringJoiner.toString()).matches()) {
+            winner = currentPlayer;
+            System.out.println(currentPlayer + " wins");
         }
     }
 
@@ -72,6 +91,9 @@ public class Connect4 {
 
     public boolean isFinished() {
         int numOfDiscs = 0;
+
+        if (winner != null) return true;
+
         for (int col = 0; col < COLUMNS; ++col) {
             numOfDiscs += getNumberOfDiscsInColumn(col);
         }
