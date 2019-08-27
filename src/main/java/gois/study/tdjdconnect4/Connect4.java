e873b8413d38db453dce7ba7cc5f0312d6373c0f
Gustavo Gois <gustavo.gois@gmail.com>
2019-08-27 21:37:12 +0100

    Requirement 4 – the game's output A few outputs should be added to let the players know the current status of the game.
    We want feedback when either an event or an error occurs within the game. The output shows the status of the board after every move.
    No output channel is specified. To make it easier, we decided to use the system standard output to print an event when it occurs. A few lines have been added on every action to let the user know about the status of the game

--- src/main/java/gois/study/tdjdconnect4/Connect4.java
+++ src/main/java/gois/study/tdjdconnect4/Connect4.java
@@ -1,11 +1,13 @@
 package gois.study.tdjdconnect4;
 
 import java.util.Arrays;
+import java.util.StringJoiner;
 
 public class Connect4 {
 
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
+    private static final String DELIMITER = "|";
 
     private Color[][] board = new Color[COLUMNS][ROWS];
     private Color currentPlayer = Color.RED;
@@ -22,6 +24,18 @@ public class Connect4 {
         } else {
             currentPlayer = Color.RED;
         }
+        System.out.println("Current turn: " + currentPlayer);
+    }
+
+    public void printBoard() {
+        for (int row = ROWS - 1; row >= 0; --row) {
+            StringJoiner stringJoiner =
+                    new StringJoiner(DELIMITER, DELIMITER, DELIMITER);
+            for (int col = 0; col < COLUMNS; ++col) {
+                stringJoiner.add(board[col][row].toString());
+            }
+            System.out.println(stringJoiner.toString());
+        }
     }
 
     public void putDisc(int column) {
@@ -29,8 +43,17 @@ public class Connect4 {
             int numOfDiscs = getNumberOfDiscsInColumn(column - 1);
             if (numOfDiscs < ROWS) {
                 board[column - 1][numOfDiscs] = currentPlayer;
+                printBoard();
                 switchPlayer();
+            } else {
+                System.out.println(numOfDiscs);
+                System.out.println("There's no room " +
+                        "for a new disc in this column");
+                printBoard();
             }
+        } else {
+            System.out.println("Column out of bounds");
+            printBoard();
         }
     }
 
